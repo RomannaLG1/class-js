83e81c1c6341f513eb5bbd333c4ad7dd940dda03
RomannaLG1 <melnykromasja@gmail.com>
2022-10-30 00:26:29 +0300

    add fetch

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,50 +1,46 @@
+
+
+
 // Add imports above this line
 // import { skillsItems } from './gallery-items';
 // // Change code below this line
-
 // console.log(skillsItems);
 import {
   fetchPopularMovies,
   fetchMovieById,
   fetchMoviesGenres,
 } from './api-service';
-
 const inputEl = document.querySelector('#search-box');
 const listEl = document.querySelector('.movies-list');
-
-fetchPopularMovies(3).then(response => {
-  for (const elm in response.data.results.genre_ids) {
-    // console.log('11111111111111111111111');
-    // console.log(response.data.results.genre_ids);
-    console.log(elm);
-  }
-
-  const itemMarkup = renderMoviesCard(response.data.results);
-  listEl.insertAdjacentHTML('beforeend', itemMarkup);
-});
-
-//fetchMovieById(299536).then(response => renderMoviesCard(response.data));
 const arrayGenres = [];
 fetchMoviesGenres().then(response => {
-  console.log(response);
+  // console.log(response);
   for (const elm of response.data.genres) {
     // console.log(elm);
     arrayGenres.push(elm);
-    console.log(elm);
   }
 });
-console.log(arrayGenres);
-// function clearListEl() {
-//   listEl.innerHTML = '';
-// }
-
-function renderMoviesCard(movies) {
-  return movies
-    .map(
-      ({ id, poster_path, title, original_title, genre_ids, release_date }) => {
-        return ` <li class='movie-info'>
+fetchPopularMovies(3).then(response => {
+  console.log(response);
+  const filmsArray = response.data.results;
+  console.log(filmsArray);
+  filmsArray.forEach(element => {
+    const newGenresArray = [];
+    const resultGenres = element.genre_ids.map(genreId => {
+      const resulIdtArray = arrayGenres.map( item => {
+        if(item.id === genreId){
+          newGenresArray.push(item.name);
+        }
+      })
+    })
+    renderMoviesCard(element, newGenresArray);
+  });
+});
+function renderMoviesCard(movie, genres) {
+ const { id, poster_path, title, original_title, release_date } = movie;
+  listEl.innerHTML +=
+         ` <li class='movie-info'>
       <a href="#" id="${id}">
-      
           <img
             class='movie-poster'
             src='https://image.tmdb.org/t/p/w500/${poster_path}'
@@ -52,15 +48,9 @@ function renderMoviesCard(movies) {
             width='395'
             height='574'
           />
-      
           <h3 class='movie-title'> ${original_title}</h3>
-          <p class='genres'>${genre_ids}</p>
-          <p class='year'>${Number.parseInt(release_date)}</p>
+          <p class='genres'>${genres.join(', ')}</p>
+          <p class='year'>${+parseInt(release_date)}</p>
       </a>
-          
-      
         </li>`;
-      }
-    )
-    .join('');
 }
